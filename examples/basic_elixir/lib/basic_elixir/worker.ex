f82e6a2132944ead2a5040199ec4d1f18e27c984
kw7oe <choongkwern@hotmail.com>
2021-10-23 17:10:26 +0800

    Update example to be consistent with opentelemetry.io docs

--- examples/basic_elixir/lib/basic_elixir/worker.ex
+++ examples/basic_elixir/lib/basic_elixir/worker.ex
@@ -1,14 +1,20 @@
 defmodule BasicElixir.Worker do
   use GenServer
   require Logger
-  require OpenTelemetry.Tracer
+  require OpenTelemetry.Tracer, as: Tracer
   require OpenTelemetry.Span
 
   # Client
   def start_link(default) when is_list(default) do
-    OpenTelemetry.Tracer.with_span "start_link" do
-      span_ctx = OpenTelemetry.Tracer.current_span_ctx
-      OpenTelemetry.Span.set_attribute(span_ctx, "hello", "world")
+    Tracer.with_span "start_link" do
+      Tracer.add_event("Nice operation!", [{"bogons", 100}])
+      Tracer.set_attributes([{:another_key, "yes"}])
+
+      Tracer.with_span "Sub operation..." do
+        Tracer.set_attributes([{:lemons_key, "five"}])
+        Tracer.add_event("Sub span event!", [])
+      end
+
       GenServer.start_link(__MODULE__, default)
     end
   end
@@ -24,7 +30,7 @@ defmodule BasicElixir.Worker do
   # Server (callbacks)
   @impl true
   def init(stack) do
-    OpenTelemetry.Tracer.with_span "init" do
+    Tracer.with_span "init" do
       Logger.info("Starting #{__MODULE__}...")
       {:ok, stack}
     end
