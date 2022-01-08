a5674a2d9206deedf69002c74e68bdeb3e59928f
kw7oe <choongkwern@hotmail.com>
2022-01-08 14:51:42 +0800

    Configure runtime config for exporter to export to otel collector in prod env

--- examples/basic_phoenix_ecto/config/runtime.exs
+++ examples/basic_phoenix_ecto/config/runtime.exs
@@ -55,6 +55,16 @@ if config_env() == :prod do
     ],
     secret_key_base: secret_key_base
 
+  config :opentelemetry, :processors,
+    otel_batch_processor: %{
+    # Using `otel` here since we are starting through docker-compose where
+    # otel refer to the hostname of the OpenCollector,
+      #
+    # If you are running it locally, kindly change it to the correct
+    # hostname such as `localhost`, `0.0.0.0` and etc.
+      exporter: {:opentelemetry_exporter, %{endpoints: [{:http, 'otel', 55681, []}]}}
+    }
+
   # ## Using releases
   #
   # If you are doing OTP releases, you need to instruct Phoenix
