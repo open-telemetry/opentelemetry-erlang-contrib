aced5c2ec00598c13aad50aa814394682901f1c1
kw7oe <choongkwern@hotmail.com>
2022-01-16 17:27:18 +0800

    Generate Posts HTML resources

--- examples/basic_phoenix_ecto/lib/demo_web/router.ex
+++ examples/basic_phoenix_ecto/lib/demo_web/router.ex
@@ -18,6 +18,7 @@ defmodule DemoWeb.Router do
     pipe_through :browser
 
     get "/", PageController, :index
+    resources "/posts", PostController
   end
 
   # Other scopes may use custom stacks.
