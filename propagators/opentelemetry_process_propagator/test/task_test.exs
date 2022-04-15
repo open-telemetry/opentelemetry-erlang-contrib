24626d84ddf915c82b95081051842274810ff88e
Bryan Naegele <bryan@fourthtime.com>
2022-04-15 16:27:36 -0600

    Conveniences for Tasks

---
+++ propagators/opentelemetry_process_propagator/test/task_test.exs
@@ -0,0 +1,164 @@
+defmodule OpentelemetryProcessPropagator.TaskTest do
+  use ExUnit.Case
+
+  alias OpentelemetryProcessPropagator.Task
+
+  require OpenTelemetry.Tracer, as: Tracer
+  require OpenTelemetry.Span
+  require Record
+
+  for {name, spec} <- Record.extract_all(from_lib: "opentelemetry/include/otel_span.hrl") do
+    Record.defrecord(name, spec)
+  end
+
+  for {name, spec} <- Record.extract_all(from_lib: "opentelemetry_api/include/opentelemetry.hrl") do
+    Record.defrecord(name, spec)
+  end
+
+  setup do
+    :application.stop(:opentelemetry)
+    :application.set_env(:opentelemetry, :tracer, :otel_tracer_default)
+
+    :application.set_env(:opentelemetry, :processors, [
+      {:otel_batch_processor, %{scheduled_delay_ms: 1}}
+    ])
+
+    :application.start(:opentelemetry)
+
+    :otel_batch_processor.set_exporter(:otel_exporter_pid, self())
+
+    :ok
+  end
+
+  test "async_stream_with_ctx" do
+    Tracer.with_span "parent span" do
+      Task.async_stream_with_ctx(["a", "b"], fn letter ->
+        Tracer.set_attribute(:letter, letter)
+
+        :ok
+      end)
+      |> Stream.run()
+    end
+
+    assert_receive {:span, span(name: "parent span", attributes: attrs)}
+
+    assert %{letter: "b"} == attributes(attrs)
+  end
+
+  test "async_stream_with_span" do
+    Tracer.with_span "parent span" do
+      Task.async_stream_with_span(["a", "b"], "task span", %{attributes: %{a: 1}}, fn letter ->
+        Tracer.set_attribute(:letter, letter)
+
+        :ok
+      end)
+      |> Stream.run()
+    end
+
+    assert_receive {:span, span(span_id: root_span_id, name: "parent span")}, 500
+
+    assert_receive {:span, span(parent_span_id: ^root_span_id, name: "task span", attributes: attrs)}, 500
+    assert %{letter: "a", a: 1} == attributes(attrs)
+
+    assert_receive {:span, span(parent_span_id: ^root_span_id, name: "task span", attributes: attrs)}, 500
+    assert %{letter: "b", a: 1} == attributes(attrs)
+  end
+
+  test "async_stream_with_linked_span" do
+    Tracer.with_span "parent span" do
+      Task.async_stream_with_linked_span(["a", "b"], "task span", %{attributes: %{a: 1}}, fn letter ->
+        Tracer.set_attribute(:letter, letter)
+
+        :ok
+      end)
+      |> Stream.run()
+    end
+
+    assert_receive {:span, span(name: "parent span")}, 500
+
+    assert_receive {:span, span(parent_span_id: :undefined, links: links, name: "task span", attributes: attrs)}, 500
+    assert %{letter: "a", a: 1} == attributes(attrs)
+    assert [] != links
+
+    assert_receive {:span, span(parent_span_id: :undefined, links: links, name: "task span", attributes: attrs)}, 500
+    assert %{letter: "b", a: 1} == attributes(attrs)
+    assert [] != links
+  end
+
+  test "async_with_ctx" do
+    Tracer.with_span "parent span", %{attributes: %{a: 1}} do
+      Task.async_with_ctx(fn ->
+        Tracer.set_attribute(:b, 2)
+
+        :ok
+      end)
+      |> Task.await()
+    end
+
+    assert_receive {:span, span(name: "parent span", attributes: attrs)}
+    assert %{a: 1, b: 2} == attributes(attrs)
+  end
+
+  test "async_with_span" do
+    Tracer.with_span "parent span" do
+      Task.async_with_span("task span", %{attributes: %{a: 1}}, fn ->
+        Tracer.set_attribute(:b, 2)
+
+        :ok
+      end)
+      |> Task.await()
+    end
+
+    assert_receive {:span, span(span_id: root_span_id, name: "parent span")}
+    assert_receive {:span, span(parent_span_id: ^root_span_id, name: "task span", attributes: attrs)}
+
+    assert %{a: 1, b: 2} == attributes(attrs)
+  end
+
+  test "async_with_linked_span" do
+    Tracer.with_span "parent span" do
+      Task.async_with_linked_span("linked task span", %{attributes: %{a: 1}}, fn ->
+        Tracer.set_attribute(:b, 2)
+
+        :ok
+      end)
+      |> Task.await()
+    end
+
+    assert_receive {:span, span(name: "parent span")}
+    assert_receive {:span, span(parent_span_id: :undefined, links: links, attributes: attrs, name: "linked task span")}
+    assert %{a: 1, b: 2} == attributes(attrs)
+    assert [] != links
+  end
+
+  test "start_with_span" do
+    Tracer.with_span "parent span" do
+      Task.start_with_span("task span", %{attributes: %{a: 1}}, fn ->
+        Tracer.set_attribute(:b, 2)
+
+        :ok
+      end)
+    end
+
+    assert_receive {:span, span(span_id: root_span_id, name: "parent span")}
+    assert_receive {:span, span(parent_span_id: ^root_span_id, attributes: attrs, name: "task span")}
+    assert %{a: 1, b: 2} == attributes(attrs)
+  end
+
+  test "start_with_linked_span" do
+    Tracer.with_span "parent span" do
+      Task.start_with_linked_span("linked task span", %{attributes: %{a: 1}}, fn ->
+        Tracer.set_attribute(:b, 2)
+
+        :ok
+      end)
+    end
+
+    assert_receive {:span, span(name: "parent span")}
+    assert_receive {:span, span(parent_span_id: :undefined, attributes: attrs, links: links, name: "linked task span")}
+    assert %{a: 1, b: 2} == attributes(attrs)
+    assert [] != links
+  end
+
+  defp attributes({_, _, _, _, attributes}), do: attributes
+end
