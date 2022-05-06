eadcec760467009252ef7ebf249234b21cebe995
Andrew Rosa <dev@andrewhr.io>
2022-05-06 10:17:45 -0300

    Fix two use cases of Ecto span propagation
    First one is related to `OpenTelemetry.Ctx` API. I've noticed in a few
    scenarios the current span of a trace may get lost after Ecto calls.
    
    Looking at the The `attach/1` typespec, it's a Ctx -> Token, while
    `dettach/1` as Token -> Ctx function. That made me assume the expected
    input of dettach is the return type of attach. Indeed, after this change
    we got the behavior of Ecto calls preserve the parent span untouched.
    
    That leads to a second bug found. When ecto does simple calls within a
    Task, due the special propagation code for preloads that means it will
    skip the current span, if any.  The solution here is to first check the
    current process.
    
    One test was added to reproduce this bug.

--- instrumentation/opentelemetry_ecto/lib/opentelemetry_ecto.ex
+++ instrumentation/opentelemetry_ecto/lib/opentelemetry_ecto.ex
@@ -108,11 +108,21 @@ defmodule OpentelemetryEcto do
       |> Map.merge(base_attributes)
       |> Map.merge(additional_attributes)
 
-    parent_context = OpentelemetryProcessPropagator.fetch_parent_ctx(1, :"$callers")
+    parent_context =
+      case OpentelemetryProcessPropagator.fetch_ctx(self()) do
+        :undefined ->
+          OpentelemetryProcessPropagator.fetch_parent_ctx(1, :"$callers")
 
-    if parent_context != :undefined do
-      OpenTelemetry.Ctx.attach(parent_context)
-    end
+        ctx ->
+          ctx
+      end
+
+    parent_token =
+      if parent_context != :undefined do
+        OpenTelemetry.Ctx.attach(parent_context)
+      else
+        :undefined
+      end
 
     s =
       OpenTelemetry.Tracer.start_span(span_name, %{
@@ -131,8 +141,8 @@ defmodule OpentelemetryEcto do
 
     OpenTelemetry.Span.end_span(s)
 
-    if parent_context != :undefined do
-      OpenTelemetry.Ctx.detach(parent_context)
+    if parent_token != :undefined do
+      OpenTelemetry.Ctx.detach(parent_token)
     end
   end
 
