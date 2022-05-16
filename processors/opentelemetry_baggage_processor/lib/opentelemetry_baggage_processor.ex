25e1b3edc83d1b7b62b5a676bf4e6150e4cf5d8b
Gustavo Aguiar <gugahoa@gmail.com>
2022-05-15 21:55:09 -0300

    remove elixir files

--- processors/opentelemetry_baggage_processor/lib/opentelemetry_baggage_processor.ex
+++
@@ -1,8 +0,0 @@
-defmodule OpentelemetryBaggageProcessor do
-  @external_resource readme = Path.join([__DIR__, "../README.md"])
-
-  @moduledoc readme
-             |> File.read!()
-             |> String.split("<!-- MDOC -->")
-             |> Enum.fetch!(1)
-end
