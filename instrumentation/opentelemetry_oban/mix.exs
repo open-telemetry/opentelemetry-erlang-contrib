2b5eb14cca2b774127766e72a1e0becb744c8a28
David White <david@rartm.co.uk>
2022-08-28 12:45:02 +0100

    Fix bug in OpentelemetryOban.insert_all/4 (#103)
    Co-authored-by: Tristan Sloughter <t@crashfast.com>

--- instrumentation/opentelemetry_oban/mix.exs
+++ instrumentation/opentelemetry_oban/mix.exs
@@ -4,7 +4,7 @@ defmodule OpentelemetryOban.MixProject do
   def project do
     [
       app: :opentelemetry_oban,
-      version: "0.2.0-rc.5",
+      version: "0.2.0-rc.6",
       elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
