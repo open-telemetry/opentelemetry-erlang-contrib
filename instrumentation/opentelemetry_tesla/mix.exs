4dc532e0779fd3e31995b980213fa7820758d8ee
Bryan Naegele <bryan@fourthtime.com>
2023-01-06 14:24:53 -0700

    Publish tesla with updated sem conventions

--- instrumentation/opentelemetry_tesla/mix.exs
+++ instrumentation/opentelemetry_tesla/mix.exs
@@ -4,7 +4,7 @@ defmodule OpentelemetryTesla.MixProject do
   def project do
     [
       app: :opentelemetry_tesla,
-      version: "2.1.0",
+      version: "2.2.0",
       elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
