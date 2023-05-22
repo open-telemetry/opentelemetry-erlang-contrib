8893af3dd5a194b241371ecbcb53bb25459f35cf
Cedric Ziel <cedric.ziel@grafana.com>
2023-05-22 15:25:34 +0200

    Update instrumentation/opentelemetry_ecto/test/opentelemetry_ecto_test.exs

--- instrumentation/opentelemetry_ecto/test/opentelemetry_ecto_test.exs
+++ instrumentation/opentelemetry_ecto/test/opentelemetry_ecto_test.exs
@@ -50,7 +50,7 @@ defmodule OpentelemetryEctoTest do
 
     assert %{
              "db.instance": "opentelemetry_ecto_test",
-             "db.name: "opentelemetry_ecto_test",
+             "db.name": "opentelemetry_ecto_test",
              "db.statement": "SELECT u0.\"id\", u0.\"email\" FROM \"users\" AS u0",
              "db.type": :sql,
              "db.url": "ecto://localhost",
