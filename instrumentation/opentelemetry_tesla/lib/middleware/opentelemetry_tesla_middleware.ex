777604300cf05324e1201985d7882fb7205318d3
Guilherme de Maio <nirev@taming-chaos.com>
2023-05-24 18:21:24 -0300

    change propagator to it uses global default

--- instrumentation/opentelemetry_tesla/lib/middleware/opentelemetry_tesla_middleware.ex
+++ instrumentation/opentelemetry_tesla/lib/middleware/opentelemetry_tesla_middleware.ex
@@ -25,15 +25,13 @@ defmodule Tesla.Middleware.OpenTelemetry do
 
   @behaviour Tesla.Middleware
 
-  @default_propagator :otel_propagator_text_map
-
   def call(env, next, opts) do
     span_name = get_span_name(env, Keyword.get(opts, :span_name))
 
     OpenTelemetry.Tracer.with_span span_name, %{kind: :client} do
       env
       |> maybe_put_additional_ok_statuses(opts[:mark_status_ok])
-      |> maybe_propagate(Keyword.get(opts, :propagator, @default_propagator))
+      |> maybe_propagate(Keyword.get(opts, :propagator, :opentelemetry.get_text_map_injector()))
       |> Tesla.run(next)
       |> set_span_attributes()
       |> handle_result()
@@ -56,7 +54,13 @@ defmodule Tesla.Middleware.OpenTelemetry do
   end
 
   defp maybe_propagate(env, :none), do: env
-  defp maybe_propagate(env, propagator), do: Tesla.put_headers(env, propagator.inject([]))
+  defp maybe_propagate(env, propagator) do
+    :otel_propagator_text_map.inject(
+      propagator,
+      env,
+      fn key, value, env -> Tesla.put_header(env, key, value) end
+    )
+  end
 
   defp maybe_put_additional_ok_statuses(env, [_ | _] = additional_ok_statuses) do
     case env.opts[:additional_ok_statuses] do
