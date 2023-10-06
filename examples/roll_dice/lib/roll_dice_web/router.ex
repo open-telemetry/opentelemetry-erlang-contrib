9d419d438e1ea2671df380f24284d501c0dfa752
Marc Delagrammatikas <marc@mechanical-orchard.com>
2023-10-06 11:27:32 -0700

    generate a phoenix API only project

---
+++ examples/roll_dice/lib/roll_dice_web/router.ex
@@ -0,0 +1,11 @@
+defmodule RollDiceWeb.Router do
+  use RollDiceWeb, :router
+
+  pipeline :api do
+    plug :accepts, ["json"]
+  end
+
+  scope "/api", RollDiceWeb do
+    pipe_through :api
+  end
+end
