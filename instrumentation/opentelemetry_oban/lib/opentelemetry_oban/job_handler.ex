7f2162a9d9769fb4c551f2980ba1bbb789e736eb
Brent Roberts <brent.roberts@podium.com>
2023-10-07 13:38:56 -0600

    Add time units

--- instrumentation/opentelemetry_oban/lib/opentelemetry_oban/job_handler.ex
+++ instrumentation/opentelemetry_oban/lib/opentelemetry_oban/job_handler.ex
@@ -107,8 +107,8 @@ defmodule OpentelemetryOban.JobHandler do
 
   defp set_measurements_attributes(%{duration: duration, queue_time: queue_time}) do
     OpenTelemetry.Tracer.set_attributes(%{
-      :"messaging.oban.duration" => System.convert_time_unit(duration, :native, :microsecond),
-      :"messaging.oban.queue_time" =>
+      :"messaging.oban.duration_us" => System.convert_time_unit(duration, :native, :microsecond),
+      :"messaging.oban.queue_time_us" =>
         System.convert_time_unit(queue_time, :nanosecond, :microsecond)
     })
   end
