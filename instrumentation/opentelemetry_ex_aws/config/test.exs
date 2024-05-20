45b47f545189e6f683c32ab44c8932b3c5a312ed
prithvihv <hvprithvi09@gmail.com>
2024-05-20 15:15:24 +0200

    fix test config

--- instrumentation/opentelemetry_ex_aws/config/test.exs
+++ instrumentation/opentelemetry_ex_aws/config/test.exs
@@ -7,8 +7,8 @@ config :opentelemetry,
 
 config :ex_aws,
   json_codec: Test.JSONCodec,
-  access_key_id: ["test_key_id"],
-  secret_access_key: ["secret_access_key"]
+  access_key_id: "testkeyid",
+  secret_access_key: "secretaccesskey"
 
 config :ex_aws, :dynamodb,
   scheme: "http://",
