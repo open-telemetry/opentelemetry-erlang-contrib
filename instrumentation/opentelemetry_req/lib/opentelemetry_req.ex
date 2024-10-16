39b7f3643afb36ac0fe07a72b92f8b483f8991a0
Bryan Naegele <bryannaegele@users.noreply.github.com>
2024-10-16 17:31:56 -0600

    Req 1.27 SemConv (#375)
    * Req 1.26 SemConv
    
    * bump semconv
    
    * Format
    
    * otel http
    
    * sem conv in main
    
    * update deps
    
    * Update deps
    
    * Update instrumentation/opentelemetry_req/lib/opentelemetry_req.ex
    
    Co-authored-by: Greg Mefford <greg@gregmefford.com>
    
    * Update instrumentation/opentelemetry_req/lib/opentelemetry_req.ex
    
    Co-authored-by: Greg Mefford <greg@gregmefford.com>
    
    * Guard against multiple headers
    
    * Bump otel
    
    * moar pipes
    
    * Update docs around path params
    
    * Remove unused opt
    
    * Align opt-in attrs with other HTTP libs
    
    * Make RC and update changelog
    
    * Dialyzer and fixes
    
    ---------
    
    Co-authored-by: Greg Mefford <greg@gregmefford.com>

--- instrumentation/opentelemetry_req/lib/opentelemetry_req.ex
+++ instrumentation/opentelemetry_req/lib/opentelemetry_req.ex
@@ -1,26 +1,109 @@
 defmodule OpentelemetryReq do
+  alias OpenTelemetry.Ctx
+
+  alias OpenTelemetry.SemConv.ErrorAttributes
+  alias OpenTelemetry.SemConv.NetworkAttributes
+  alias OpenTelemetry.SemConv.ServerAttributes
+  alias OpenTelemetry.SemConv.UserAgentAttributes
+  alias OpenTelemetry.SemConv.Incubating.HTTPAttributes
+  alias OpenTelemetry.SemConv.Incubating.URLAttributes
+
+  alias OpenTelemetry.Tracer
+  alias OpenTelemetry.SemanticConventions.Trace
+  require Trace
+  require Tracer
+  require Logger
+
+  opt_ins = [
+    HTTPAttributes.http_request_body_size(),
+    HTTPAttributes.http_response_body_size(),
+    NetworkAttributes.network_transport(),
+    URLAttributes.url_scheme(),
+    URLAttributes.url_template(),
+    UserAgentAttributes.user_agent_original()
+  ]
+
+  @options_schema NimbleOptions.new!(
+                    opt_in_attrs: [
+                      type: {:list, {:in, opt_ins}},
+                      default: [],
+                      type_spec: quote(do: opt_in_attrs()),
+                      doc: """
+                      Opt-in and experimental attributes. Use semantic conventions library to ensure compatability, e.g. `[{HTTPAttributes.http_request_body_size(), true}]`
+
+                      #{Enum.map_join(opt_ins, "\n\n", &"  * `#{inspect(&1)}`")}
+                      """
+                    ],
+                    propagate_trace_headers: [
+                      type: :boolean,
+                      default: false,
+                      doc: "Trace headers will be propagated"
+                    ],
+                    request_header_attrs: [
+                      type: {:list, :string},
+                      default: [],
+                      doc: "List of request headers to add as attributes. (lowercase)"
+                    ],
+                    response_header_attrs: [
+                      type: {:list, :string},
+                      default: [],
+                      doc: "List of response headers to add as attributes. (lowercase)"
+                    ],
+                    span_name: [
+                      type: {:or, [:atom, nil, :string]},
+                      default: nil,
+                      doc: "User defined span name override"
+                    ]
+                  )
+
+  @typedoc "Use semantic conventions library to ensure compatability, e.g. `HTTPAttributes.http_request_body_size()`"
+  @type opt_in_attr() ::
+          unquote(HTTPAttributes.http_request_body_size())
+          | unquote(HTTPAttributes.http_response_body_size())
+          | unquote(NetworkAttributes.network_transport())
+          | unquote(URLAttributes.url_scheme())
+          | unquote(URLAttributes.url_template())
+          | unquote(UserAgentAttributes.user_agent_original())
+
+  @type opt_in_attrs() :: [opt_in_attr()]
+
+  @type options() :: [unquote(NimbleOptions.option_typespec(@options_schema))]
+
   @moduledoc """
-  Wraps the request in an opentelemetry span. Span names must be parameterized, so the
-  `req_path_params` module and step should be registered before this step. This step is
-  expected by default and an error will be raised if the path params option is
-  not set for the request.
+  Wraps a Req request in an opentelemetry span.
 
   Spans are not created until the request is completed or errored.
 
-  ## Request Options
+  ## Req Path Params
 
-    * `:span_name` - `String.t()` if provided, overrides the span name. Defaults to `nil`.
-    * `:no_path_params` - `boolean()` when set to `true` no path params are expected for the request. Defaults to `false`
-    * `:propagate_trace_ctx` - `boolean()` when set to `true`, trace headers will be propagated. Defaults to `false`
+  It is strongly encouraged to use the [`put_path_params` step](https://hexdocs.pm/req/Req.Steps.html#put_path_params/1) option.
+  This allows the span name to include the `{target}` portion of the span name described in the
+  [HTTP Span Name guidelines](https://opentelemetry.io/docs/specs/semconv/http/http-spans/#name).
 
-  ### Example with path_params
+  > #### Requirements {: .info}
+  >
+  > * `path_params` option should be set along with a templated path. Only `:colon` style is supported
+  > * `URLAttributes.url_template()` opt-in attribute must be set to `true`
 
+  ## Semantic Conventions
+
+  All available required and recommended [Client HTTP Span](https://opentelemetry.io/docs/specs/semconv/http/http-spans/#http-client) semantic conventions are implemented.
+  Supported opt-in and experimental attributes can be configured using the `opt_in_attrs` option.
+
+  ## Options
+
+  ### Opt-in Semantic Convention Attributes
+
+  Otel SemConv requires users to explicitly opt in for any attribute with a
+  requirement level of `opt-in` or `experimental`. To ensure compatability, always use the
+  SemConv attribute.
+
+  Example:
   ```
   client =
     Req.new()
     |> OpentelemetryReq.attach(
-      base_url: "http://localhost:4000",
-      propagate_trace_ctx: true
+      opt_in_attrs: [SemConv.URLAttributes.url_template()]
     )
 
   client
@@ -30,39 +113,76 @@ defmodule OpentelemetryReq do
   )
   ```
 
-  ### Example without path_params
+  Request and response header attributes are opt-in and can be set with the
+  `request_header_attrs` and `response_header_attrs` options. Values should be lower-case.
 
+  ### Trace Header Propagation
+
+  By default, trace propagation headers are not injected to requests. There are
+  two options available to propagate trace headers:
+
+    * set `propagate_trace_headers` option to `true` when attaching or in the call
+    * manually use `:otel_propagator_text_map.inject/1`
+
+  Example:
   ```
   client =
     Req.new()
-    |> OpentelemetryReq.attach(
-      base_url: "http://localhost:4000",
-      propagate_trace_ctx: true,
-      no_path_params: true
-    )
+    |> OpentelemetryReq.attach(propagate_trace_headers: true)
 
-  client
-  |> Req.get(
-    url: "/api/users"
-  )
+  # or
+
+  client =
+    Req.new()
+    |> OpentelemetryReq.attach()
+
+  Req.get(client, "/", propagate_trace_headers: true)
   ```
-  If you don't set `path_params` the request will raise.
+
+  ### Span Name Override
+
+  The span name can be overridden by setting the `span_name` option in the call.
+
+  Example:
+  ```
+  client =
+    Req.new()
+    |> OpentelemetryReq.attach()
+
+  Req.get(client, "/", span_name: "custom")
+  ```
+
+  > #### Option Precedence {: .info}
+  >
+  > Options passed in a request take precedence over those passed in `attach/1`.
+  >
   """
 
-  alias OpenTelemetry.Tracer
-  alias OpenTelemetry.SemanticConventions.Trace
-  require Trace
-  require Tracer
-  require Logger
-
+  @spec attach(Req.Request.t(), options()) :: Req.Request.t()
   def attach(%Req.Request{} = request, options \\ []) do
+    config =
+      options
+      |> NimbleOptions.validate!(@options_schema)
+      |> Enum.into(%{})
+      |> then(fn config ->
+        if Enum.member?(config.opt_in_attrs, URLAttributes.url_template()) do
+          Map.put(config, :url_template_enabled, true)
+        else
+          Map.put(config, :url_template_enabled, false)
+        end
+      end)
+
     request
-    |> Req.Request.register_options([:span_name, :no_path_params, :propagate_trace_ctx])
-    |> Req.Request.merge_options(options)
+    |> Req.Request.put_private(:otel, config)
+    |> Req.Request.register_options([
+      :propagate_trace_headers,
+      :request_header_attrs,
+      :response_header_attrs,
+      :span_name
+    ])
     |> Req.Request.append_request_steps(
-      require_path_params: &require_path_params_option/1,
       start_span: &start_span/1,
-      put_trace_headers: &maybe_put_trace_headers/1
+      put_trace_headers: &propagate_trace_headers/1
     )
     |> Req.Request.prepend_response_steps(otel_end_span: &end_span/1)
     |> Req.Request.prepend_error_steps(otel_end_span: &end_errored_span/1)
@@ -86,14 +206,31 @@ defmodule OpentelemetryReq do
   end
 
   defp end_span({request, %Req.Response{} = response}) do
-    attrs =
-      Map.put(%{}, Trace.http_status_code(), response.status)
-      |> maybe_append_resp_content_length(response)
+    config = Req.Request.get_private(request, :otel)
 
-    Tracer.set_attributes(attrs)
+    opt_in =
+      %{
+        HTTPAttributes.http_response_body_size() => extract_response_body_size(response)
+      }
+      |> Map.take(config.opt_in_attrs)
 
     if response.status >= 400 do
-      OpenTelemetry.Tracer.set_status(OpenTelemetry.status(:error, ""))
+      Tracer.set_status(OpenTelemetry.status(:error, ""))
+
+      %{
+        HTTPAttributes.http_response_status_code() => response.status,
+        ErrorAttributes.error_type() => to_string(response.status)
+      }
+      |> set_resp_header_attrs(response, request.options)
+      |> Map.merge(opt_in)
+      |> Tracer.set_attributes()
+    else
+      %{
+        HTTPAttributes.http_response_status_code() => response.status
+      }
+      |> set_resp_header_attrs(response, request.options)
+      |> Map.merge(opt_in)
+      |> Tracer.set_attributes()
     end
 
     OpenTelemetry.Tracer.end_span()
@@ -105,12 +242,16 @@ defmodule OpentelemetryReq do
   end
 
   defp end_errored_span({request, exception}) do
-    OpenTelemetry.Tracer.set_status(OpenTelemetry.status(:error, format_exception(exception)))
+    Tracer.set_status(OpenTelemetry.status(:error, format_exception(exception)))
 
-    OpenTelemetry.Tracer.end_span()
+    Tracer.set_attributes(%{
+      ErrorAttributes.error_type() => exception.__struct__
+    })
+
+    Tracer.end_span()
 
     Process.delete(:otel_parent_ctx)
-    |> OpenTelemetry.Ctx.attach()
+    |> Ctx.attach()
 
     {request, exception}
   end
@@ -124,11 +265,21 @@ defmodule OpentelemetryReq do
   defp span_name(request) do
     case request.options[:span_name] do
       nil ->
-        method = http_method(request.method)
+        config = Req.Request.get_private(request, :otel)
 
-        case Req.Request.get_private(request, :path_params_template) do
-          nil -> "HTTP #{method}"
-          params_template -> "#{params_template}"
+        if config.span_name do
+          config.span_name
+        else
+          method = parse_method(request.method)
+
+          if config.url_template_enabled do
+            case Req.Request.get_private(request, :path_params_template) do
+              nil -> method
+              params_template -> "#{method} #{params_template}"
+            end
+          else
+            method
+          end
         end
 
       span_name ->
@@ -139,16 +290,29 @@ defmodule OpentelemetryReq do
   defp build_req_attrs(request) do
     uri = request.url
     url = sanitize_url(uri)
+    config = Req.Request.get_private(request, :otel)
+
+    opt_in = %{
+      HTTPAttributes.http_request_body_size() => extract_request_body_size(request),
+      NetworkAttributes.network_transport() => :tcp,
+      URLAttributes.url_scheme() => extract_scheme(uri),
+      URLAttributes.url_template() => extract_url_template(request),
+      UserAgentAttributes.user_agent_original() => extract_user_agent(request)
+    }
 
     %{
-      Trace.http_method() => http_method(request.method),
-      Trace.http_url() => url,
-      Trace.http_target() => uri.path,
-      Trace.net_host_name() => uri.host,
-      Trace.http_scheme() => uri.scheme
+      HTTPAttributes.http_request_method() => parse_method(request.method),
+      ServerAttributes.server_address() => uri.host,
+      ServerAttributes.server_port() => extract_port(uri),
+      URLAttributes.url_full() => url
     }
-    |> maybe_append_req_content_length(request)
-    |> maybe_append_retry_count(request)
+    |> set_retry_count(request)
+    |> set_req_header_attrs(request)
+    |> then(fn attrs ->
+      opt_in
+      |> Map.take(config.opt_in_attrs)
+      |> Map.merge(attrs)
+    end)
   end
 
   defp sanitize_url(uri) do
@@ -156,81 +320,124 @@ defmodule OpentelemetryReq do
     |> URI.to_string()
   end
 
-  defp maybe_append_req_content_length(attrs, req) do
-    case Req.Request.get_header(req, "content-length") do
-      [] ->
-        attrs
+  defp extract_port(%{port: port}) when is_integer(port), do: port
 
-      [length] ->
-        Map.put(attrs, Trace.http_request_content_length(), length)
+  defp extract_port(%{scheme: scheme}) do
+    case scheme do
+      nil -> 80
+      "http" -> 80
+      "https" -> 443
+      _ -> 80
     end
   end
 
-  defp maybe_append_resp_content_length(attrs, req) do
+  defp extract_scheme(%{scheme: scheme}) do
+    case scheme do
+      nil -> :http
+      "http" -> :http
+      "https" -> :https
+      _ -> :http
+    end
+  end
+
+  defp extract_url_template(request) do
+    Req.Request.get_private(request, :path_params_template, "")
+  end
+
+  defp extract_user_agent(request) do
+    case Req.Request.get_header(request, "user-agent") do
+      [] ->
+        ""
+
+      [user_agent | _] ->
+        user_agent
+    end
+  end
+
+  defp extract_response_body_size(req) do
     case Req.Response.get_header(req, "content-length") do
       [] ->
-        attrs
+        0
 
-      [length] ->
-        Map.put(attrs, Trace.http_response_content_length(), length)
+      [length_str | _] when is_binary(length_str) ->
+        # Req sets this as a string but should be an integer
+        # https://www.rfc-editor.org/rfc/rfc9110.html#name-content-length
+        # https://opentelemetry.io/docs/specs/semconv/attributes-registry/http
+        String.to_integer(length_str)
     end
   end
 
-  defp maybe_append_retry_count(attrs, req) do
+  defp extract_request_body_size(req) do
+    case Req.Request.get_header(req, "content-length") do
+      [] ->
+        0
+
+      [length_str | _] when is_binary(length_str) ->
+        # Req sets this as a string but should be an integer
+        # https://www.rfc-editor.org/rfc/rfc9110.html#name-content-length
+        # https://opentelemetry.io/docs/specs/semconv/attributes-registry/http
+        String.to_integer(length_str)
+    end
+  end
+
+  defp set_retry_count(attrs, req) do
     retry_count = Req.Request.get_private(req, :req_retry_count, 0)
 
     if retry_count > 0 do
-      Map.put(attrs, Trace.http_retry_count(), retry_count)
+      Map.put(attrs, HTTPAttributes.http_request_resend_count(), retry_count)
     else
       attrs
     end
   end
 
-  defp http_method(method) do
+  defp parse_method(method) do
     case method do
-      :get -> :GET
-      :head -> :HEAD
-      :post -> :POST
-      :patch -> :PATCH
-      :put -> :PUT
-      :delete -> :DELETE
-      :connect -> :CONNECT
-      :options -> :OPTIONS
-      :trace -> :TRACE
+      :connect -> HTTPAttributes.http_request_method_values().connect
+      :delete -> HTTPAttributes.http_request_method_values().delete
+      :get -> HTTPAttributes.http_request_method_values().get
+      :head -> HTTPAttributes.http_request_method_values().head
+      :options -> HTTPAttributes.http_request_method_values().options
+      :patch -> HTTPAttributes.http_request_method_values().patch
+      :post -> HTTPAttributes.http_request_method_values().post
+      :put -> HTTPAttributes.http_request_method_values().put
+      :trace -> HTTPAttributes.http_request_method_values().trace
     end
   end
 
-  defp maybe_put_trace_headers(request) do
-    if request.options[:propagate_trace_ctx] do
-      propagator = :opentelemetry.get_text_map_injector()
-      headers_to_inject = :otel_propagator_text_map.inject(propagator, [], &[{&1, &2} | &3])
+  defp propagate_trace_headers(request) do
+    should_inject =
+      Req.Request.get_option(
+        request,
+        :propagate_trace_headers,
+        Req.Request.get_private(request, :otel)[:propagate_trace_headers]
+      )
 
-      Enum.reduce(headers_to_inject, request, fn {name, value}, acc ->
-        Req.Request.put_header(acc, name, value)
-      end)
+    if should_inject do
+      Req.Request.put_headers(request, :otel_propagator_text_map.inject([]))
     else
       request
     end
   end
 
-  defp require_path_params_option(request) do
-    if !request.options[:no_path_params] and !request.options[:path_params] do
-      {Req.Request.halt(request), __MODULE__.PathParamsOptionError.new()}
-    else
-      request
-    end
+  defp set_req_header_attrs(attrs, req) do
+    Map.merge(
+      attrs,
+      :otel_http.extract_headers_attributes(
+        :request,
+        req.headers,
+        Map.get(req.options, :request_header_attrs, [])
+      )
+    )
   end
 
-  defmodule PathParamsOptionError do
-    defexception [:message]
-
-    def new do
-      %__MODULE__{}
-    end
-
-    @impl true
-    def message(_) do
-      ":path_params option must be set"
-    end
+  defp set_resp_header_attrs(attrs, resp, options) do
+    Map.merge(
+      attrs,
+      :otel_http.extract_headers_attributes(
+        :response,
+        resp.headers,
+        Map.get(options, :response_header_attrs, [])
+      )
+    )
   end
 end
