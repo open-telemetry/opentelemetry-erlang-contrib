2f9109c74974507e14c8808edbd9996d931f0138
Dan Schultzer <1254724+danschultzer@users.noreply.github.com>
2024-12-13 19:41:56 -0800

    Refactor live view test

--- instrumentation/opentelemetry_phoenix/test/opentelemetry_phoenix_test.exs
+++ instrumentation/opentelemetry_phoenix/test/opentelemetry_phoenix_test.exs
@@ -7,7 +7,63 @@ defmodule OpentelemetryPhoenixTest do
   require Record
 
   alias OpenTelemetry.SemConv.ExceptionAttributes
-  alias PhoenixLiveViewMeta, as: LiveViewMeta
+
+  @socket Phoenix.LiveView.Utils.configure_socket(
+            %Phoenix.LiveView.Socket{
+              endpoint: NnnnWeb.Endpoint,
+              router: NnnnnWeb.Router,
+              view: NnnnnWeb.MyTestLive
+            },
+            %{
+              connect_params: %{},
+              connect_info: %{},
+              root_view: NnnnnWeb.MyTestLive,
+              live_temp: %{}
+            },
+            nil,
+            %{},
+            URI.parse("https://localhost:4000/live?foo=bar")
+          )
+
+  @telemetry_meta_base %{
+    socket: @socket,
+    params: %{"foo" => "bar"},
+    uri: "https://localhost:4000/live?foo=bar",
+    telemetry_span_context: :dummy_ref
+  }
+
+  @telemetry_meta_handle_params Map.put(@telemetry_meta_base, :event, "hello")
+
+  @exception_meta %{
+    reason: %RuntimeError{message: "stop"},
+    stacktrace: [
+      {NnnnnWeb.MyTestLive, :handle_params, 3,
+       [
+         file: ~c"lib/nnnnn_web/live/my_test_live.ex",
+         line: 28,
+         error_info: %{module: Exception}
+       ]}
+    ],
+    kind: :error
+  }
+
+  @telemetry_meta %{
+    mount: %{
+      start: @telemetry_meta_base,
+      stop: @telemetry_meta_base,
+      exception: @telemetry_meta_base
+    },
+    handle_params: %{
+      start: @telemetry_meta_base,
+      stop: @telemetry_meta_base,
+      exception: Map.merge(@telemetry_meta_base, @exception_meta)
+    },
+    handle_event: %{
+      start: @telemetry_meta_handle_params,
+      stop: @telemetry_meta_handle_params,
+      exception: Map.merge(@telemetry_meta_handle_params, @exception_meta)
+    }
+  }
 
   for {name, spec} <- Record.extract_all(from_lib: "opentelemetry/include/otel_span.hrl") do
     Record.defrecord(name, spec)
@@ -33,13 +89,13 @@ defmodule OpentelemetryPhoenixTest do
     :telemetry.execute(
       [:phoenix, :live_view, :mount, :start],
       %{system_time: System.system_time()},
-      LiveViewMeta.mount_start()
+      @telemetry_meta.mount.start
     )
 
     :telemetry.execute(
       [:phoenix, :live_view, :mount, :stop],
       %{system_time: System.system_time()},
-      LiveViewMeta.mount_stop()
+      @telemetry_meta.mount.stop
     )
 
     assert_receive {:span,
@@ -55,13 +111,13 @@ defmodule OpentelemetryPhoenixTest do
     :telemetry.execute(
       [:phoenix, :live_view, :handle_params, :start],
       %{system_time: System.system_time()},
-      LiveViewMeta.handle_params_start()
+      @telemetry_meta.handle_params.start
     )
 
     :telemetry.execute(
       [:phoenix, :live_view, :handle_params, :stop],
       %{system_time: System.system_time()},
-      LiveViewMeta.handle_params_stop()
+      @telemetry_meta.handle_params.stop
     )
 
     assert_receive {:span,
@@ -77,13 +133,13 @@ defmodule OpentelemetryPhoenixTest do
     :telemetry.execute(
       [:phoenix, :live_view, :handle_event, :start],
       %{system_time: System.system_time()},
-      LiveViewMeta.handle_event_start()
+      @telemetry_meta.handle_event.start
     )
 
     :telemetry.execute(
       [:phoenix, :live_view, :handle_event, :stop],
       %{system_time: System.system_time()},
-      LiveViewMeta.handle_event_stop()
+      @telemetry_meta.handle_event.stop
     )
 
     assert_receive {:span,
@@ -99,25 +155,25 @@ defmodule OpentelemetryPhoenixTest do
     :telemetry.execute(
       [:phoenix, :live_view, :mount, :start],
       %{system_time: System.system_time()},
-      LiveViewMeta.mount_start(:exception)
+      @telemetry_meta.mount.start
     )
 
     :telemetry.execute(
       [:phoenix, :live_view, :mount, :stop],
       %{system_time: System.system_time()},
-      LiveViewMeta.mount_stop(:exception)
+      @telemetry_meta.mount.stop
     )
 
     :telemetry.execute(
       [:phoenix, :live_view, :handle_params, :start],
       %{system_time: System.system_time()},
-      LiveViewMeta.handle_params_start(:exception)
+      @telemetry_meta.handle_params.start
     )
 
     :telemetry.execute(
       [:phoenix, :live_view, :handle_params, :exception],
       %{system_time: System.system_time()},
-      LiveViewMeta.handle_params_exception(:exception)
+      @telemetry_meta.handle_params.exception
     )
 
     assert_receive {:span,
@@ -156,13 +212,13 @@ defmodule OpentelemetryPhoenixTest do
     :telemetry.execute(
       [:phoenix, :live_view, :handle_event, :start],
       %{system_time: System.system_time()},
-      LiveViewMeta.handle_event_start(:exception)
+      @telemetry_meta.handle_event.start
     )
 
     :telemetry.execute(
       [:phoenix, :live_view, :handle_event, :exception],
       %{system_time: System.system_time()},
-      LiveViewMeta.handle_event_exception(:exception)
+      @telemetry_meta.handle_event.exception
     )
 
     assert_receive {:span,
