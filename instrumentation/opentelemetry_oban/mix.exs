2088825db589117d4b34cf029fe2e90876a2e925
Gonzalo <456459+grzuy@users.noreply.github.com>
2025-11-04 11:18:34 -0300

    test: keep jason (no longer included by oban) for postgrex

--- instrumentation/opentelemetry_oban/mix.exs
+++ instrumentation/opentelemetry_oban/mix.exs
@@ -52,7 +52,9 @@ defmodule OpentelemetryOban.MixProject do
       {:opentelemetry_exporter, "~> 1.0", only: [:test]},
       {:telemetry, "~> 0.4 or ~> 1.0"},
       {:ex_doc, "~> 0.38", only: [:dev], runtime: false},
-      {:postgrex, ">= 0.0.0", only: [:dev, :test]}
+      {:postgrex, ">= 0.0.0", only: [:dev, :test]},
+      # Needed by postgrex
+      {:jason, "~> 1.4", only: :test}
     ]
   end
 
