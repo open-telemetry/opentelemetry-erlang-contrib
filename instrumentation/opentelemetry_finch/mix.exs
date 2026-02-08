50aed821f2ca358cf55bfc4da7c2244c8af596a7
Yordis Prieto <yordis.prieto@gmail.com>
2026-02-08 08:16:46 -0500

    chore(opentelemetry_finch): add nimble_options dependency for improved configuration handling

--- instrumentation/opentelemetry_finch/mix.exs
+++ instrumentation/opentelemetry_finch/mix.exs
@@ -60,6 +60,7 @@ defmodule OpentelemetryFinch.MixProject do
       {:opentelemetry, "~> 1.5", only: [:dev, :test]},
       {:opentelemetry_exporter, "~> 1.8", only: [:dev, :test]},
       {:otel_http, "~> 0.2"},
+      {:nimble_options, "~> 1.1"},
       {:ex_doc, "~> 0.38", only: [:dev], runtime: false},
       {:finch, "~> 0.20", only: [:dev, :test]},
       {:dialyxir, "~> 1.4", only: [:dev, :test], runtime: false},
