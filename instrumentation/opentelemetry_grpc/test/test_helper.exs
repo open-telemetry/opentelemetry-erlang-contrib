2dbdee0c48b06813749bb96f062a5c3b23c7bc43
Yordis Prieto <yordis.prieto@gmail.com>
2026-07-21 22:52:23 -0400

    feat(opentelemetry_grpc): support grpc 1.0 (#697)
    * feat(opentelemetry_grpc): support grpc 1.0
    
    Signed-off-by: Yordis Prieto <yordis.prieto@gmail.com>
    
    * fix(opentelemetry_grpc): relax pinned dev/test dep versions
    
    Exact-pinning grpc/grpc_server/gun in the dev/test constraints forces
    unnecessary lockstep bumps for patch releases that don't affect
    compatibility.
    
    Signed-off-by: Yordis Prieto <yordis.prieto@gmail.com>
    
    ---------
    
    Signed-off-by: Yordis Prieto <yordis.prieto@gmail.com>

--- instrumentation/opentelemetry_grpc/test/test_helper.exs
+++ instrumentation/opentelemetry_grpc/test/test_helper.exs
@@ -1,4 +1,11 @@
 Application.ensure_all_started(:opentelemetry)
 Application.ensure_all_started(:opentelemetry_api)
 
+# grpc 0.x expects the host application to run the client connection
+# supervisor; grpc 1.0 starts it in its own supervision tree
+case DynamicSupervisor.start_link(strategy: :one_for_one, name: GRPC.Client.Supervisor) do
+  {:ok, _pid} -> :ok
+  {:error, {:already_started, _pid}} -> :ok
+end
+
 ExUnit.start()
